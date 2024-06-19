b88d13c0db7437381b45757a1470f30a372194f9
10senura <senuraindeewara@gmail.com>
2024-06-19 13:31:07 +0530

    calculator

---
+++ add.js
@@ -0,0 +1,17 @@
+function calc() {
+    let number1 = new Number(document.getElementById("num1").value);
+    let number2 = new Number(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+    let output = document.getElementById("output");
+
+    console.log(operator);
+    
+    switch (operator) {
+        case "+": output.innerHTML = (number1 + number2); break;
+        case "-": output.innerHTML = (number1 - number2); break;
+        case "*": output.innerHTML = (number1 * number2); break;
+        case "/": output.innerHTML = (number1 / number2); break;
+        default: output = "Choose option";
+    }
+
+}
